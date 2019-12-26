2e30376eaa171d24e3e84ba574a76f851d49c355
zhangxu <zhangxu261@gmail.com>
2019-12-26 15:19:33 +0800

    去除多数据源

--- pear/src/main/java/com/lyfen/pear/framework/config/RuoYiConfig.java
+++ pear/src/main/java/com/lyfen/pear/framework/config/RuoYiConfig.java
@@ -12,66 +12,9 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "ruoyi")
 public class RuoYiConfig {
     /**
-     * 项目名称
-     */
-    private String name;
-
-    /**
-     * 版本
-     */
-    private String version;
-
-    /**
-     * 版权年份
-     */
-    private String copyrightYear;
-
-    /**
-     * 实例演示开关
-     */
-    private boolean demoEnabled;
-
-    /**
      * 上传路径
      */
     private static String profile;
-
-    /**
-     * 获取地址开关
-     */
-    private static boolean addressEnabled;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getCopyrightYear() {
-        return copyrightYear;
-    }
-
-    public void setCopyrightYear(String copyrightYear) {
-        this.copyrightYear = copyrightYear;
-    }
-
-    public boolean isDemoEnabled() {
-        return demoEnabled;
-    }
-
-    public void setDemoEnabled(boolean demoEnabled) {
-        this.demoEnabled = demoEnabled;
-    }
 
     public static String getProfile() {
         return profile;
@@ -79,14 +22,6 @@ public class RuoYiConfig {
 
     public void setProfile(String profile) {
         RuoYiConfig.profile = profile;
-    }
-
-    public static boolean isAddressEnabled() {
-        return addressEnabled;
-    }
-
-    public void setAddressEnabled(boolean addressEnabled) {
-        RuoYiConfig.addressEnabled = addressEnabled;
     }
 
     /**
